1f690d501d21e8b40e8d58f47e174f3f1562c6e9
bfh687 <bfh687@uw.edu>
2022-04-23 19:16:03 -0700

    fix misnamed imports

--- utilities/exports.js
+++ utilities/exports.js
@@ -1,8 +1,8 @@
 const pool = require("./sql_conn.js");
-const cred = require("./credentialing");
+const cred = require("./cred");
 const generateHash = cred.generateHash;
 const generateSalt = cred.generateSalt;
-const validation = require("./validationUtils.js");
+const validation = require("./validation.js");
 const sendEmail = require("./email.js").sendEmail;
 
 module.exports = {
